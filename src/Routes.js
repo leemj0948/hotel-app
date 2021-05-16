cad84ef7c170d6ce6201e440ceee3463bf8543f9
MJ <mj@MJui-MacBookPro.local>
2021-05-16 20:50:03 +0900

    ESLint prettier 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import MapPage from './MapPage/MapPage';
 // import Main from './Pages/Main/Main';
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        {/* <Route exact path="/" component={Main} /> */}
+        <Route exact path="/map" component={MapPage} />
         {/* <Route exact path="/" component={Main} /> */}
       </Switch>
     </Router>
