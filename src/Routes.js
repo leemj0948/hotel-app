7295291943b19ef541b66eeaab77ee21fee5cb9f
je-Rebecca <67889991+je-Rebecca@users.noreply.github.com>
2021-07-03 04:56:17 -0400

    updated routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import Banner from './components/banner/Banner.components';
-import Header from './components/header/Header.components';
+import Header from './Main/Components/header/Header.components';
+// import Header from './components/header/Header.components';
 import MapMainPage from './Map/MapMainPage';
-import Main from './pages/Main/Main';
+import Main from './Main/Main';
+// import Main from './pages/Main/Main';
 import Login from './Login/Login';
+import Footer from './Main/Components/footer/Footer';
 
 const Routes = () => {
   // function scrollE() {
